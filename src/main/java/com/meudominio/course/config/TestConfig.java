b088492fcf271206cc1b3803cf7e94b9b47434e6
Thiago Goncalves <mg.az.ul@hotmail.com>
2022-01-24 13:15:16 -0300

    Payment, one-to-one association

--- src/main/java/com/meudominio/course/config/TestConfig.java
+++ src/main/java/com/meudominio/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.meudominio.course.entities.Category;
 import com.meudominio.course.entities.Order;
 import com.meudominio.course.entities.OrderItem;
+import com.meudominio.course.entities.Payment;
 import com.meudominio.course.entities.Product;
 import com.meudominio.course.entities.User;
 import com.meudominio.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z") , o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
